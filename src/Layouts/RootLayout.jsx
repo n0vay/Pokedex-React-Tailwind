608ef0bf99b675d4aa5c421e024e2ed89603c38d
n0vay <koshantanu@gmail.com>
2023-11-13 17:11:36 -0500

    Updated Navbar

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -2,19 +2,51 @@ import { NavLink, Outlet } from "react-router-dom";
 import logo from "../assets/images/logo.png";
 const RootLayout = () => {
   return (
-    <div className="root-layout">
+    <div className="">
       <header>
-        <nav>
-          <h1>My Pokedex is here</h1>
-          <div className="flex">
-            <img src={logo} className="h-12" alt="pokemon logo" />
-          </div>
-          <NavLink to="/" className="">
-            Discover
-          </NavLink>
-          <NavLink to="/search">Search</NavLink>
-          <NavLink to="/pagination">Pagination</NavLink>
-        </nav>
+        <div className=" bg-orange-400 p-2">
+          <nav className=" flex justify-between">
+            <div className="flex flex-row">
+              <div className="flex">
+                <div>
+                  <img src={logo} className="h-12" alt="pokemon logo" />
+                </div>
+                <div className="flex  p-3">
+                  <NavLink to="/" className="px-2">
+                    <div className="bg-gray-700 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded">
+                      Discover
+                    </div>
+                  </NavLink>
+                  <NavLink to="/search" className="px-2">
+                    <div className="bg-gray-700 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded">
+                      Search
+                    </div>
+                  </NavLink>
+                  <NavLink to="/pagination" className="px-2">
+                    <div className="bg-gray-700 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded">
+                      Pagination
+                    </div>
+                  </NavLink>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-row justify-end ">
+              <div className="flex p-3">
+                <NavLink to="/search" className="px-2">
+                  <div className="bg-gray-700 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded">
+                    Log In
+                  </div>
+                </NavLink>
+                <NavLink to="/pagination" className="px-2">
+                  <div className="bg-gray-700 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded">
+                    Sign Up
+                  </div>
+                </NavLink>
+              </div>
+            </div>
+          </nav>
+        </div>
+        <div className="bg-gray-700 h-2" />
       </header>
       <main>
         <Outlet />
